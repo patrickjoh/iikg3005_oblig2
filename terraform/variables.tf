9ee1b025c3ed3d7a7ca4205a70b96c1dfd73b62f
patrickjoh <patrick.johannessen@hotmail.com>
2023-11-04 12:44:19 +0100

    add: Files for main the main infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+# Resource group
+variable rg_name {
+    type = string
+    description = "Name of the resource group"
+}
+
+
+# Storage account
+variable sa_name {
+    type = string
+    description = "Name of the storage account"
+}
