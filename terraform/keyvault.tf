560722226e96e5679e96d9c557e2e4bbdd190bc9
patrickjoh <patrick.johannessen@hotmail.com>
2023-11-04 19:42:22 +0100

    change: Retentions days for key vault

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -6,7 +6,7 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
+  soft_delete_retention_days  = 8
   purge_protection_enabled    = false
 
 
