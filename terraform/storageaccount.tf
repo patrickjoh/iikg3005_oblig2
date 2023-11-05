d007c07e4a552e4ddc4838baccebc1595f31853c
patrickjoh <patrick.johannessen@hotmail.com>
2023-11-05 10:20:18 +0100

    Testing workflow

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -2,8 +2,8 @@ resource "azurerm_storage_account" "sa" {
   name                     = "${var.base_name}${var.sa_name}${random_string.random_string.result}${local.workspace_suffix}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = "Standar"
+  account_replication_type = "GRS"
 }
 
 resource "azurerm_storage_container" "sc" {
