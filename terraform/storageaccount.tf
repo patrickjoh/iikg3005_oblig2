cdb033bd72a0022b5148e26161ff7d9e60cb5ab4
patrickjoh <patrick.johannessen@hotmail.com>
2023-11-05 11:35:12 +0100

    Testing of tflint
    Changed account_tier back to "Standard"

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_account" "sa" {
   name                     = "${var.base_name}${var.sa_name}${random_string.random_string.result}${local.workspace_suffix}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standar"
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
